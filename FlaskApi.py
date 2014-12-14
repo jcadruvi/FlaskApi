74b298887eda1f12d2c71cb4cef2b943cb3b1dc1
Josh Cadruvi <jcadruvi@imatchative.com>
2014-12-14 09:45:49 -0800

    Adding the users API.

--- FlaskApi.py
+++ FlaskApi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/api/users', methods=['GET'])
+def users():
+    userList = {
+        "users": [
+        { "id": 1, "name": "John Doe" },
+        { "id": 2, "name": "Jane Doe" },
+        { "id": 3, "name": "John Smith" },
+        { "id": 4, "name": "John Smith" }
+    ]}
+    return jsonify(userList)
 
 if __name__ == '__main__':
     app.run()
