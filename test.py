7830b339c66137b10b468ccb62f15ec26b9c4f55
Josh Cadruvi <jcadruvi@imatchative.com>
2015-01-27 17:53:58 -0800

    Refactored self.app to be what is used to call the api.

--- test.py
+++ test.py
@@ -3,17 +3,16 @@ import FlaskApi
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
-        #self.app = create_app()
-        self.client = FlaskApi.app.test_client()
+        self.app = FlaskApi.app.test_client()
         self.assertTrue(FlaskApi.app is not None)
-        self.assertTrue(self.client is not None)
+        self.assertTrue(self.app is not None)
 
     def tearDown(self):
         pass
 
     def test_user_api(self):
-        json = self.client.get('/api/users')
-        print json
+        json = self.app.get('/api/users')
+        print json.data
 
 
 if __name__ == '__main__':
